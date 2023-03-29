0585bc5698a7dc31529d2ccdd38e300135626747
JSW-JW <seungwon.jeon40@gmail.com>
2023-03-30 00:26:59 +0900

    init: migrate maven to gradle

---
+++ backend/src/main/java/com/example/webnovelservice/MainApplication.java
@@ -0,0 +1,15 @@
+package com.example.webnovelservice;
+
+import com.example.webnovelservice.config.AppProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
---
+++ backend/src/main/java/com/example/webnovelservice/model/AuthProvider.java
@@ -0,0 +1,8 @@
+package com.example.webnovelservice.model;
+
+public enum  AuthProvider {
+    local,
+    facebook,
+    google,
+    github
+}
