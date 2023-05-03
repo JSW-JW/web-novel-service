a0f928602c956a7b71fe9bb96e782e93b5929f29
JSW-JW <seungwon.jeon40@gmail.com>
2023-05-03 22:48:12 +0900

    Feat: best novels 조회 swagger placeholder 추가
    e.g: showCaseTypeIds: [1, 2]
    
    Resolves: #25

--- backend/src/main/java/com/example/webnovelservice/novel/dto/response/NovelDto.java
+++ backend/src/main/java/com/example/webnovelservice/novel/dto/response/NovelDto.java
@@ -9,14 +9,17 @@ public class NovelDto {
 	public NovelDto() {}
 	private Long id;
 	private String title;
+
+	private String thumbnailUrl;
 	private String genre;
 	private String description;
 
 	private String showcaseType;
 
-	public NovelDto(Long id, String title, String genre, String description, String showcaseType) {
+	public NovelDto(Long id, String title, String genre, String description, String showcaseType, String thumbnailUrl) {
 		this.id = id;
 		this.title = title;
+		this.thumbnailUrl = thumbnailUrl;
 		this.genre = genre;
 		this.description = description;
 		this.showcaseType = showcaseType;
