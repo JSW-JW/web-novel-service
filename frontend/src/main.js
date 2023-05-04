bbddb6f4c3d730578aa3fafe86623d7459562200
JSW-JW <seungwon.jeon40@gmail.com>
2023-05-04 21:54:18 +0900

    Feat: 소설 상세 vue page
    소설 정보
    챕터 리스트 정보
    
    Ref: #29

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+
+// Use provide and inject to set up a global property
+app.config.globalProperties.$apiBaseUrl = 'http://localhost:8080/api/v1';
+
+app.mount('#app');
