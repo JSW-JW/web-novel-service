52410be9bcfdc63bd34e5b78ca03de935288b75d
JSW-JW <seungwon.jeon40@gmail.com>
2023-05-14 22:59:40 +0900

    Test: pass non-sensitive env file for ci/cd
    pass non-sensitive env file for production
    
    Ref: #39

--- backend/src/main/java/com/example/webnovelservice/MainApplication.java
+++ backend/src/main/java/com/example/webnovelservice/MainApplication.java
@@ -1,5 +1,8 @@
 package com.example.webnovelservice;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,6 +15,13 @@ import com.example.webnovelservice.commons.properties.AppProperties;
 @EnableCaching
 public class MainApplication {
 
+	@Value("${S3_ACCESSKEY}")
+	private String test;
+
+	@PostConstruct
+	public void test() {
+		System.out.println(">>> " + test);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
