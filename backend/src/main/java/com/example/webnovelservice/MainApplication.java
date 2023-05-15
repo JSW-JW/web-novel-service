3a73d84ea7639259b545603a47f1e492170128f5
JSW-JW <seungwon.jeon40@gmail.com>
2023-05-15 16:32:46 +0900

    Test: ci/cd test change deploy.sh file
    deploy.sh file 앱 실행 시 profile option 추가
    
    Ref: #39

--- backend/src/main/java/com/example/webnovelservice/MainApplication.java
+++ backend/src/main/java/com/example/webnovelservice/MainApplication.java
@@ -1,8 +1,5 @@
 package com.example.webnovelservice;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,14 +11,6 @@ import com.example.webnovelservice.commons.properties.AppProperties;
 @EnableConfigurationProperties(AppProperties.class)
 @EnableCaching
 public class MainApplication {
-
-	@Value("${S3_ACCESSKEY}")
-	private String test;
-
-	@PostConstruct
-	public void test() {
-		System.out.println(">>> " + test);
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
